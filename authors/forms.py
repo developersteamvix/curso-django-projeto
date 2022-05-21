a8f55b475431ddbea422442c3a7a17c5ee9dc8ff
developersteamvix <developersteam.vix@gmail.com>
2022-05-21 06:32:37 -0300

    is_valid and save to save the form data in the database

--- authors/forms.py
+++ authors/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -6,6 +8,20 @@ from django.core.exceptions import ValidationError
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
     field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError(
+            (
+                'Password must have at least one uppercase letter, '
+                'one lowercase letter and one number. The length should be '
+                ' at least 8 characters.'
+            ),
+            code='Invalid',
+        )
 
 
 def add_placeholder(field, placeholder_val):
@@ -35,6 +51,7 @@ class RegisterForm(forms.ModelForm):
             'one lowercase letter and one number. The length should be '
             ' at least 8 characters.'
         ),
+        validators=[strong_password, ],
     )
     password2 = forms.CharField(
         required=True,
