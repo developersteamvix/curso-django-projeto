bc10c90615f3d137f8808a7ddd25dd94d6c4b0fd
developersteamvix <developersteam.vix@gmail.com>
2022-06-30 19:55:01 -0300

    Reusing Class Based View in inheritance to delete records.

--- authors/views/all.py
+++ authors/views/all.py
@@ -113,26 +113,3 @@ def dashboard_view(request):
             'recipes': recipes,
         },
     )
-
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_delete_view(request):
-    if not request.POST:
-        raise Http404()
-
-    POST = request.POST
-    id = POST.get('id')
-
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-
-    recipe.delete()
-    messages.success(request, 'Deleted successfully.')
-
-    return redirect(reverse('authors:dashboard'))
