ed007071b1e111c64d9d3adfcc040ad8ebd428ac
developersteamvix <developersteam.vix@gmail.com>
2022-06-30 22:29:37 -0300

    Moving the revenue detail FBV to the CBV DetailView.

--- recipes/views.py
+++ recipes/views.py
@@ -2,8 +2,7 @@ import os
 
 from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from utils.pagination import make_pagination
 
 from recipes.models import Recipe
@@ -110,10 +109,13 @@ class RecipeListViewSearch(RecipeListViewBase):
         return ctx
 
 
-def recipe(request, id):
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/pages/recipe-view.html'
 
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe,
-        'is_detail_page': True,
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_detail_page'] = True
+
+        return context
