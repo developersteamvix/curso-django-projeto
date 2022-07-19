5e39e1b47bcf4af6f31cab5e9b05cedb9b590299
developersteamvix <developersteam.vix@gmail.com>
2022-07-19 10:41:42 -0300

    Organizing the settings.py file into a Python Package.

---
+++ project/settings/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+import imp
+
+from .environment import *  # isort:skip
+from .installed_apps import *  # isort:skip
+from .middlewares import *  # isort:skip
+
+from .assets import *  # isort:skip
+from .databases import *  # isort:skip
+from .i18n import *  # isort:skip
+from .messages import *  # isort:skip
+from .security import *  # isort:skip
+from .templates import *  # isort:skip
+
+from .debug_toolbar import *  # isort:skip
