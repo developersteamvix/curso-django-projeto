880b8a951c4580b8da54eae0c09ba8710d7b361b
developersteamvix <developersteam.vix@gmail.com>
2022-07-22 00:28:53 -0300

    Using gettext for translations in views.

--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,7 @@ from django.forms.models import model_to_dict
 from django.http import Http404, JsonResponse
 from django.shortcuts import render
 from django.utils import translation
+from django.utils.translation import gettext as _
 from django.views.generic import DetailView, ListView
 from tag.models import Tag
 from utils.pagination import make_pagination
@@ -99,9 +100,10 @@ class RecipeListViewCategory(RecipeListViewBase):
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
+        category_translation = _('Categoria')
 
         ctx.update({
-            'title': f'{ctx.get("recipes")[0].category.name} - Category | ',
+            'title': f'{ctx.get("recipes")[0].category.name} - {category_translation} | ',  # noqa: E501
         })
 
         return ctx
